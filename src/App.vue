b104cf10e65184553c1afb4f45d7688d0b0331ed
mjf <JianFeng.Mao@aliyun.com>
2023-05-21 22:50:51 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ProductList />
+  </div>
+</template>
+
+<script>
+import ProductList from './components/ProductList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductList
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+ul,
+li {
+  list-style: none;
+}
+</style>
